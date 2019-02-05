85ccc34c11b1e608a2c572931fa9425d060eb491
antoinequellier <antoinecquellier@gmail.com>
2019-02-05 15:23:57 +0000

    navbar colors change on icon + check contrast with background images

--- source/assets/javascripts/components/button.js
+++ source/assets/javascripts/components/button.js
@@ -1,17 +1,30 @@
 const changeButton = () => {
   console.log('hello from changebutton');
   const button = document.querySelector('.oval-button');
+  const whiteItems = document.querySelectorAll('.white');
   if (button) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight/2.5) {
-        button.classList.remove('white');
-        button.classList.add('gray');
+        whiteItems.forEach((item) => {
+          removeWhite(item);
+        });
       } else {
-        button.classList.remove('gray');
-        button.classList.add('white');
+        whiteItems.forEach((item) => {
+          addWhite(item);
+        });
       }
     });
   }
+}
+
+const removeWhite = (el) => {
+  el.classList.remove('white');
+  el.classList.add('gray');
+}
+
+const addWhite = (el) => {
+  el.classList.add('white');
+  el.classList.remove('gray');
 }
 
 export { changeButton };
--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -2,6 +2,7 @@ import './application';
 import { navbarScroll } from './components/navbar';
 import { initExpandTogglers } from './components/expand';
 import { changeButton } from './components/button';
+// import { fadeOut } from './components/banner';
 
 navbarScroll();
 initExpandTogglers();
