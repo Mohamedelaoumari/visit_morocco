8a4a00de6b3662937dc44a2d5a910411240d2ab3
antoinequellier <antoinecquellier@gmail.com>
2019-02-06 14:32:24 +0000

    carousel designed

--- source/assets/javascripts/components/slick.js
+++ source/assets/javascripts/components/slick.js
@@ -2,10 +2,18 @@ import $ from 'jquery';
 import 'slick-carousel';
 import 'slick-carousel/slick/slick.css';
 
+
+const prevArrow = '<button class="btn btn-link p-2 slick-control slick-arrow slick-prev"><i class="fas fa-chevron-circle-left fa-lg"></i>'
+const nextArrow = '<button class="btn btn-link p-2 slick-control slick-arrow slick-next"><i class="fas fa-chevron-circle-right fa-lg"></i></button>';
+
 const initSlider = () => {
   console.log('hellof rom slick');
   $('.single-item').slick({
-    'arrows': true
+    arrows: true,
+    dots: true,
+    dotsClass: 'slick-dots',
+    prevArrow: prevArrow,
+    nextArrow: nextArrow
   });
 }
 
